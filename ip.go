19ca59b4c6abf4f892a8b905cc9f3f1f37b24a32
Paul Kramme <pjkramme@gmail.com>
2017-05-23 15:13:15 +0200

    Add log entry when service starts

--- ip.go
+++ ip.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-  "net/http"
-  "fmt"
-  "flag"
-  "log"
-  "os"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-  listenaddr := flag.String("listen", ":80", "Address on which IP Service listens")
-  loglocation := flag.String("log", "./ipservice.log", "Location of the logfile")
-  flag.Parse()
+	listenaddr := flag.String("listen", ":80", "Address on which IP Service listens")
+	loglocation := flag.String("log", "./ipservice.log", "Location of the logfile")
+	flag.Parse()
 
-  f, err := os.OpenFile(*loglocation, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile(*loglocation, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 	log.SetOutput(f)
 
-  fmt.Println("IP Service Started")
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, r.RemoteAddr)
-    log.Printf("Request from %s", r.RemoteAddr)
-  })
-  http.ListenAndServe(*listenaddr, nil)
+	fmt.Println("IP Service Started")
+	log.Println("IP Service Started")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, r.RemoteAddr)
+		log.Printf("Request from %s", r.RemoteAddr)
+	})
+	http.ListenAndServe(*listenaddr, nil)
 }
